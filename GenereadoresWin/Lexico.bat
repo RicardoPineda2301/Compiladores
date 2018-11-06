c2befe19af360046307e8adc8f2c76608161762a
Ricardo Pineda <41968210+RicardoPineda2301@users.noreply.github.com>
2018-11-06 14:20:36 -0600

    Create Lexico.bat

---
+++ GenereadoresWin/Lexico.bat
@@ -0,0 +1,6 @@
+SET JAVA_HOME="C:\Program Files\Java\jdk1.8.0_181\bin"
+SET PATH=%JAVA_HOME%;%PATH%
+SET CLASSPATH=%JAVA_HOME%;
+cd "C:\Universidad\Sexto Semestre\Compiladores\ProyectoFinal\ProyectoFinal\src\Analizadores"
+java -jar "C:\Universidad\Sexto Semestre\Compiladores\JFlex\jflex-1.6.1\lib\jflex-1.6.1.jar" A_Lexico.jflex
+pause
