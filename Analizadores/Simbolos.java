292df7f64039490f5cf0219306f0d25923c54e01
Ricardo Pineda <41968210+RicardoPineda2301@users.noreply.github.com>
2018-11-06 14:15:39 -0600

    Update Simbolos.java

--- Analizadores/Simbolos.java
+++ Analizadores/Simbolos.java
@@ -8,35 +8,105 @@ package Analizadores;
 /** CUP generated class containing symbol constants. */
 public class Simbolos {
   /* terminals */
-  public static final int ALPHA = 12;
-  public static final int PAREN_C = 7;
-  public static final int INT = 8;
-  public static final int PAREN_A = 6;
-  public static final int CLASSPROGRAM = 2;
-  public static final int MAIN = 5;
-  public static final int BRACKET_C = 11;
-  public static final int BOOLEAN = 9;
-  public static final int BRACKET_A = 10;
-  public static final int DIGIT = 13;
+  public static final int CBRACE = 22;
+  public static final int GTEQ = 14;
+  public static final int LTEQ = 13;
+  public static final int OBRACK = 23;
+  public static final int HEX = 44;
+  public static final int FLS = 41;
+  public static final int PUNTOCOMA = 20;
+  public static final int FOR = 29;
+  public static final int MINUS = 3;
+  public static final int OBRACE = 21;
+  public static final int RTN = 43;
+  public static final int STRGN = 47;
+  public static final int OROR = 17;
+  public static final int LT = 10;
+  public static final int PEQ = 7;
+  public static final int CLASS = 32;
+  public static final int DIV = 5;
+  public static final int ANDAND = 16;
+  public static final int NUM = 48;
+  public static final int DBLPUNTO = 25;
+  public static final int PLUS = 2;
+  public static final int MAIN = 42;
+  public static final int IF = 27;
+  public static final int BOOLEAN = 37;
   public static final int EOF = 0;
-  public static final int LLAVE_C = 4;
-  public static final int LLAVE_A = 3;
   public static final int error = 1;
+  public static final int COMA = 26;
+  public static final int EQEQ = 12;
+  public static final int BREAK = 30;
+  public static final int VOID = 38;
+  public static final int TRT = 40;
+  public static final int EQ = 6;
+  public static final int CLL = 45;
+  public static final int TIMES = 4;
+  public static final int NEG = 9;
+  public static final int ELSE = 28;
+  public static final int LETTER = 46;
+  public static final int FUNC = 34;
+  public static final int IDNT = 36;
+  public static final int INTG = 39;
+  public static final int LEQ = 8;
+  public static final int PARC = 19;
+  public static final int PARA = 18;
+  public static final int CNT = 35;
+  public static final int GT = 11;
+  public static final int PRINT = 31;
+  public static final int CBRACK = 24;
+  public static final int PRGRM = 33;
+  public static final int NOTEQ = 15;
   public static final String[] terminalNames = new String[] {
   "EOF",
   "error",
-  "CLASSPROGRAM",
-  "LLAVE_A",
-  "LLAVE_C",
-  "MAIN",
-  "PAREN_A",
-  "PAREN_C",
-  "INT",
+  "PLUS",
+  "MINUS",
+  "TIMES",
+  "DIV",
+  "EQ",
+  "PEQ",
+  "LEQ",
+  "NEG",
+  "LT",
+  "GT",
+  "EQEQ",
+  "LTEQ",
+  "GTEQ",
+  "NOTEQ",
+  "ANDAND",
+  "OROR",
+  "PARA",
+  "PARC",
+  "PUNTOCOMA",
+  "OBRACE",
+  "CBRACE",
+  "OBRACK",
+  "CBRACK",
+  "DBLPUNTO",
+  "COMA",
+  "IF",
+  "ELSE",
+  "FOR",
+  "BREAK",
+  "PRINT",
+  "CLASS",
+  "PRGRM",
+  "FUNC",
+  "CNT",
+  "IDNT",
   "BOOLEAN",
-  "BRACKET_A",
-  "BRACKET_C",
-  "ALPHA",
-  "DIGIT"
+  "VOID",
+  "INTG",
+  "TRT",
+  "FLS",
+  "MAIN",
+  "RTN",
+  "HEX",
+  "CLL",
+  "LETTER",
+  "STRGN",
+  "NUM"
   };
 }
 
