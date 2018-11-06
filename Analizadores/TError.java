a8aeff179dc56ddffa4bcefe8f8d0a8900cd3136
Ricardo Pineda <41968210+RicardoPineda2301@users.noreply.github.com>
2018-11-06 14:15:57 -0600

    Update TError.java

--- Analizadores/TError.java
+++ Analizadores/TError.java
@@ -5,21 +5,20 @@
  */
 package Analizadores;
 
+/**
+ *
+ * @author Ricardo
+ */
 public class TError {
-    String lexema, tipo, descripcion;
-    int linea, columna;
+    String lexema,tipo, descripcion;
+    double linea,columna;
     
-    public TError(String le, int li, int co, String t, String de){
-    
+    public TError(String le, double li, double co, String t, String de){
         lexema = le;
         linea = li;
         columna = co;
         tipo = t;
         descripcion = de;
-        
     }
-
-    TError(String lexema, double fila, double columna, String error_Sintactico, String caracter_no_esperado) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    
 }
